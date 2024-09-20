5f1d9eefc5ddaf1ab66ec93207463afbd2ee1f60
Alessandro Fusta Moro <alessandro.fustamoro@gmail.com>
2024-09-20 14:48:13 +0200

    update

---
+++ v.1.0.0/script/EEA_download(for).R
@@ -0,0 +1,35 @@
+library(doParallel)
+library(foreach)
+library(httr)
+registerDoParallel(cores=detectCores())
+
+base_url <- "https://fme.discomap.eea.europa.eu/fmedatastreaming/AirQualityDownload/AQData_Extract.fmw"
+
+for(i in c(1,5,7,8,10,35,38,6001)) { 
+           parameters <- list(
+             CountryCode = "IT",
+             CityName = "",
+             Pollutant = i,
+             Year_from = 2014,
+             Year_to = 2023,
+             Source = "All", 
+             Output = "TEXT",
+             TimeCoverage = "Year",
+             token = "8f3a54b3e7054080813237004b35694fbff43580" #it may change?
+           )
+           
+           # Construct the full URL
+           url <- paste(base_url, "?", paste0(names(parameters), "=", unlist(parameters), collapse = "&"), sep = "")
+           
+           response <- GET(url)
+           content_text <- content(response, "text", encoding = "UTF-8")
+           download_links <- unlist(strsplit(content_text,"\r\n"))
+           download_links <- sub("^[\uFEFF]", "", download_links)
+           if (length(download_links)!=0) {
+             for (j in 1:length(download_links)) {
+               download.file(download_links[j],
+                             paste0("data/raw/",
+                                    basename(download_links[j])))
+             } 
+           }
+         }
--- v.1.0.0/script/EEA_download.R
+++ v.1.0.0/script/EEA_download.R
@@ -8,16 +8,18 @@ registerDoParallel(cores=detectCores())
 base_url <- "https://fme.discomap.eea.europa.eu/fmedatastreaming/AirQualityDownload/AQData_Extract.fmw"
 
 # Define parameters
-foreach (i = c(1,5,7,8,10,35,38,6001)) %dopar% { 
+foreach (i = c(1,5,7,8,10,35,38,6001),
+         .packages = c("httr")) %dopar% { 
   parameters <- list(
     CountryCode = "IT",
     CityName = "",
     Pollutant = i,
     Year_from = 2013,
     Year_to = 2023,
-    Source = "All", #"or E1a" or "E1b" (?)
+    Source = "All", 
     Output = "TEXT",
-    TimeCoverage = "Year"
+    TimeCoverage = "Year",
+    token = "8f3a54b3e7054080813237004b35694fbff43580" #it may change?
   )
   
   # Construct the full URL
@@ -27,15 +29,11 @@ foreach (i = c(1,5,7,8,10,35,38,6001)) %dopar% {
   content_text <- content(response, "text", encoding = "UTF-8")
   download_links <- unlist(strsplit(content_text,"\r\n"))
   download_links <- sub("^[\uFEFF]", "", download_links)
-  # already_links <- substr(download_links,
-  #                         nchar(download_links)-(28+nchar(as.character(i))),
-  #                                                200) %in% list.files("GRINS/R_GRINS/data/EEA/raw")
-  # download_links <- download_links[already_links==FALSE]
   if (length(download_links)!=0) {
-    for (i in 1:length(download_links)) {
-      download.file(download_links[i],
+    for (j in 1:length(download_links)) {
+      download.file(download_links[j],
                     paste0("data/raw/",
-                           basename(download_links[i])))
+                           basename(download_links[j])))
     } 
   }
 }
